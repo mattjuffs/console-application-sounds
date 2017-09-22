035b0b241bdefd8414557b6d6737618322d96a7e
Matt Juffs <matt@slickhouse.com>
2017-09-22 08:46:39 +0100

    Initial commit

---
+++ ConsoleApplicationSounds/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.IO;
+using System.Speech.Synthesis;
+
+namespace ConsoleApplicationSounds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BeepAtMe();
+            SpeakToMe();
+        }
+
+        static void BeepAtMe()
+        {
+            Console.Beep(37, 1000);
+            Console.Beep(32767, 1000);
+            Console.Read();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Beep();
+                System.Threading.Thread.Sleep(1000);
+            }
+
+            Console.Beep();
+            Console.Beep();
+            Console.Read();
+        }
+
+        static void SpeakToMe()
+        {
+            // see: https://stackoverflow.com/a/14460543/63100
+
+            // Initialize a new instance of the SpeechSynthesizer.
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+
+            // Configure the audio output. 
+            synth.SetOutputToDefaultAudioDevice();
+            synth.Rate = 0;
+
+            // Speak a string.
+            synth.Speak("Hello World");
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+    }
+}
